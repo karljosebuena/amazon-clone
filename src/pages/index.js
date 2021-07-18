90c983af05ef72c55193fab0fd970740a8ea1b1a
Karl Jose Buena <karljosebuena@gmail.com>
2021-07-18 23:04:05 +0800

    Completed Banner Feed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
 import Head from "next/head";
-import Header from "../components/Header"
+import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
 
       {/* Header */}
-      <Header>
+      <Header />
 
-      </Header>
+      <main className="max-max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
 
-      {/* Banner */}
-
-      {/* Products Feed */}
+        {/* Products Feed */}
+      </main>
     </div>
   );
 }
